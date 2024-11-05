d762240c306be5ee8505729d7fe1453c82dfe7aa
idriouic <ilias.driouich@amadeus.com>
2024-11-05 12:36:41 +0100

    added setup + toml

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+    
+    
+setup(
+    name='travel_pii_anonymisation',
+    version='0.1.0',
+    author='Ilias',
+    author_email='ilias.driouich@amadeus.com',
+    description='A package for pseudonymizing travel-specific PII data',
+    long_description=open('readme.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/AmadeusITGroup/Travel-specific-PIIs-pseudonymization',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'pandas>=1.1.0',
+        'numpy>=1.19.0',
+        'faker>=8.0.0',
+    ],
+    tests_require=[
+        'pytest>=6.0.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'travel_pii=tspii:main',
+    ],
+    },
+    python_requires='>=3.6',
+)
