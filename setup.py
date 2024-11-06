ddebf9eb41047fb8b7af917b4a213d2d1519225c
idriouic <ilias.driouich@amadeus.com>
2024-11-06 11:23:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setup(
     name='travel_pii_anonymisation',
-    version='0.2',
+    version='0.2.1',
     author='Ilias',
     author_email='ilias.driouich@amadeus.com',
     description='A package for pseudonymizing travel-specific PII data',
